bac35caa38f4985500cc2ad6834705cc1320f1eb
IDahok <contato.dahok@gmail.com>
2025-03-30 16:00:36 -0300

    Featured: Ajuste de comunicação do DB com Back-end via Azure

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,12 @@
 import { Pool } from 'pg';
 
+// Configuração do pool de conexões
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  host: '20.206.149.115',
+  user: 'postgres',
+  password: '123456',
+  database: 'postgres',
+  port: 5432,
 });
 
 export default pool;
